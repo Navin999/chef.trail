80f41b48289d9e8623c8dca3b84e9ba725b8fd4e
navin999 <sadaramnavin@yahoo.co.in>
2016-01-22 21:34:46 -0800

    Restarting apache service

--- r1/apache2_pkg.rb
+++ r1/apache2_pkg.rb
@@ -2,9 +2,9 @@ package "apache2" do
  action :install
 end
 
-# service "apache2" do
-#  action [:enable, :start]
-# end
+service "apache2" do
+  action [:enable, :restart]
+end
 
 # file "/var/www/index.html" do
 #  source "index.html"
