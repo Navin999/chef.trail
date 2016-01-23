7a8fbdd63576386df4b3852619997f009af255d5
navin999 <sadaramnavin@yahoo.co.in>
2016-01-22 21:13:41 -0800

    Installing apache2

---
+++ r1/apache2_pkg.rb
@@ -0,0 +1,15 @@
+package "apache2" do
+ action :install
+end
+
+# service "apache2" do
+#  action [:enable, :start]
+# end
+
+# file "/var/www/index.html" do
+#  source "index.html"
+#  mode "0644"
+# end
+
+
+
